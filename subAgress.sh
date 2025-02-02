86ee5911f67781c46d28d3d58354c9fd4da4aca8
root <vikramzeus98@gmail.com>
2025-02-02 17:54:24 +0530

    Added link parser

--- subAgress.sh
+++ subAgress.sh
@@ -2,18 +2,59 @@
 
 usage(){
 	cat <<EOF
-	Usage
+	Usage ./
 	-u  : Url provided for subfinder and dnsx to be reconned
-	
+	-l  : Load File to ingest from.	
 EOF
 }
 
 
-while getopts ":u:" opts; do
+
+fileCheck(){
+		if [[ -f $1 ]]; then
+			file="$1"
+			echo "loading file $1"
+
+		else
+			echo "Not a file"
+			exit 1		
+		fi
+}
+
+linkParser(){
+	link="$1"
+	echo $link | grep -Eiwq "^(https?:\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+	if [[ $? -eq 0 ]]; then
+		isLink=0
+	else
+		isLink=1
+	fi
+}
+
+linkCheck(){
+		link="$1"
+		linkParser "$link"
+		if [[ "$isLink" -eq 0 ]]; then
+			echo "Is a link"	
+		else
+			echo "Is not a link"
+			exit 1
+		fi	
+}
+while getopts ":u:l:" opts; do
 	case $opts in
 		u)
-			link=$OPTARG
-			echo $link	
+			link="$OPTARG"
+			if [[ -n $OPTARG ]]; then
+				linkCheck $link	
+			fi
+			;;
+		l)
+			file="$OPTARG"
+			if [[ -n $OPTARG ]] ; then
+				fileCheck $file
+				
+			fi
 			;;
 		\?)
 			echo "Invalid option"
